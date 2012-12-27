cedb223f30965c8dce918a65bce28660473f49ed
David Yu <lewen.yu@gmail.com>
2012-12-27 16:21:53 -0500

    first commit

---
+++ Entity.hx
@@ -0,0 +1,37 @@
+package com.artemis;
+
+/**
+ *
+ * The entity class.
+ *  originally written by Arni Arent
+ *  ported to HaXe by Lewen Yu
+ *
+ */
+
+class Entity {
+    private var id : Int;
+    private var componentBits : Int;
+    private var systemBits : Int;
+
+    private var world //: World;
+    private var entityManager //: EntityManager;
+
+    private var uuid : int; //implement this 
+
+    public function new( World, id ) {
+        this.world = world;
+        this.id = id;
+    }
+
+    public function getId() : Int {
+        return id;
+    }
+
+    public function getComponentBits() : Int {
+        return componentBits;
+    }
+
+    public function getSystemBits() : Int {
+        return systemBits;
+    }
+}

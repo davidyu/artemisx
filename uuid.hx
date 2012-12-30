9122240a034491010215d59a91d3ba14ff62d29b
David Yu <lewen.yu@gmail.com>
2012-12-29 23:35:08 -0500

    start work on uuid

---
+++ uuid.hx
@@ -0,0 +1,20 @@
+package com.artemis.utils
+
+/**
+ *
+ * The entity class.
+ *  inspired partially by Robert Kieffer's Math.uuid.js
+ *  ported to HaXe by Lewen Yu
+ *
+ */
+
+
+class Uuid {
+
+    //fast UUID generation based on RFC4122v4
+    public static function getUUID( ?seed : Int ) {
+        if ( seed.isNull() ) {
+            seed = //random seed
+        }
+    }
+}

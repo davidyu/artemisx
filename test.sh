5addfce99ec620ce419a20c861815b8e14361734
David Yu <lewen.yu@gmail.com>
2013-01-08 21:46:17 -0500

    removed duplicates and put back cleanup to test code

--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
 TESTPATH=src/com/artemis/test
+
+# copy files; hacky
+TESTFILES=$TESTPATH/*.hx
+for f in $TESTFILES
+do
+    cp $f $TESTPATH/..
+done
+
 haxe $TESTPATH/test.hxml
 neko $TESTPATH/test.n
+
+# cleanup
+for f in $TESTFILES
+do
+    rm $TESTPATH/../$(basename $f)
+done

6b0ef6ad99698c02b9250baa173a1a0e6920fd11
David Yu <lewen.yu@gmail.com>
2013-01-08 21:43:38 -0500

    tests broken/while trying to test ComponentManager

--- src/com/artemis/ComponentMapper.hx
+++ src/com/artemis/ComponentMapper.hx
@@ -8,12 +8,18 @@ import haxe.macro.Type;
 class ComponentMapper<A:Component> implements haxe.rtti.Generic
 {
     private var type:ComponentType;
-    private var classType:Class<A>
+    private var classType:Class<Dynamic>
     private var components:TArray<Component>;
 
     private function new(type:Class<Dynamic>, world:World)
     {
         this.type = ComponentType.getTypeFor(type);
-        components = world;
+        //components = world.getComponentManager().getComponentsByType(this.type)
+        this.classType = type;
+    }
+
+    public function get( e : Entity ) : Component
+    {
+        
     }
 }

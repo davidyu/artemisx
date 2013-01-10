ce7ced40f308a338a09e0dce6d57f5ff3a55b89d
David Yu <lewen.yu@gmail.com>
2013-01-10 16:04:53 -0500

    a better system test

--- src/com/artemis/test/TestWorld.hx
+++ src/com/artemis/test/TestWorld.hx
@@ -1,14 +1,27 @@
 package com.artemis.test;
 
 import com.artemis.World;
+import com.artemis.EntitySystem;
+
+private class DummySystemA extends EntitySystem {}
+private class DummySystemB extends EntitySystem {}
 
 class TestWorld extends haxe.unit.TestCase {
-    public function testBasic() {
+    public function testSystem() {
         var w : World = new World();
-        var e : EntitySystem = new EntitySystem();
-        w.setSystemToProcess( e );
-        var e2 : EntitySystem = w.getSystem( EntitySystem );
 
-        assertEquals( e, e2 );
+        var a : DummySystemA = new DummySystemA();
+        var b : DummySystemB = new DummySystemB();
+
+        w.setSystemToProcess( a );
+        w.setSystemToProcess( b );
+
+        var a2 : DummySystemA = w.getSystem( DummySystemA );
+        var b2 : DummySystemB = w.getSystem( DummySystemB );
+
+        //if this worked a == a2 and b == b2
+
+        assertEquals( a, a2 );
+        assertEquals( b, b2 );
     }
 }

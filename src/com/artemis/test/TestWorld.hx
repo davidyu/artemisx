e30b3dcf354aed5334643e57de529f6bfffdfb34
David Yu <lewen.yu@gmail.com>
2013-01-10 15:58:54 -0500

    test getSystem setSystem; works as expected. See TestWorld.hx

--- src/com/artemis/test/TestWorld.hx
+++ src/com/artemis/test/TestWorld.hx
@@ -5,5 +5,10 @@ import com.artemis.World;
 class TestWorld extends haxe.unit.TestCase {
     public function testBasic() {
         var w : World = new World();
+        var e : EntitySystem = new EntitySystem();
+        w.setSystemToProcess( e );
+        var e2 : EntitySystem = w.getSystem( EntitySystem );
+
+        assertEquals( e, e2 );
     }
 }
